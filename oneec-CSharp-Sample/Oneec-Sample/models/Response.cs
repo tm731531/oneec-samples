f8466cc82a13df8c415a6c50de9bb577a000eb7b
Tom Ting <tm731531@hotmail.com>
2022-04-19 15:53:05 +0800

    add C# version

---
+++ oneec-CSharp-Sample/Oneec-Sample/models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oneec_Sample.models
+{
+    public class Response
+    {
+        public int status { get; set; }
+        public object message { get; set; }
+        public string data { get; set; }
+    }
+
+}
